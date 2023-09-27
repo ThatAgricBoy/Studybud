0bb65377d676dc86efbfbf570b95804841188335
ThatAgricBoy <samueljohn3999@gmail.com>
2023-09-27 10:36:53 +0100

    topic count

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -62,23 +62,25 @@ def registerUser(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) | 
-        Q(name__icontains=q) | 
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
         Q(description__icontains=q)
-        )
+    )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
-    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    room_messages = Message.objects.filter(
+        Q(room__topic__name__icontains=q))[0:3]
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages': room_messages}
+    context = {'rooms': rooms, 'topics': topics,
+               'room_count': room_count, 'room_messages': room_messages}
     return render(request, 'base/home.html', context)
-
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    room_messages = room.message_set.all()
+    room_messages = room.message_set.all().order_by('-created')
     participants = room.participant.all()
 
     if request.method == 'POST':
@@ -94,29 +96,14 @@ def room(request, pk):
     return render(request, 'base/room.html', context)
 
 
-def userProfile(request, pk):
-    user = User.objects.get(id=pk)
-    rooms = user.room_set.all()
-    room_messages = user.message_set.all()
-    topics = Topic.objects.all()
-
-    context = {'user': user, 'rooms': rooms, 'room_messages': room_messages, 'topics': topics}
-    return render(request, 'base/profile.html', context)
-
-
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
-
-
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-        return redirect('home')
-
+            form.save()
+            return redirect('home')
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
@@ -143,7 +130,7 @@ def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
     
     if request.user != room.host:
-        return HttpResponse('You are not allowed here!')
+        return HttpResponse('You are not alowed here!')
 
     if request.method == 'POST':
         room.delete()
